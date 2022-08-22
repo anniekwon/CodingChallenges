86e06c8c66016193e875e35745049b0986216cfe
Annie Kwon <annie.s.kwon@gmail.com>
2022-08-22 19:35:52 -0400

    merged solution

--- mergedsortedlists.ts
+++ mergedsortedlists.ts
@@ -1,6 +1,5 @@
 /*
-
-2) Mergek Sorted Lists
+2) Merge Sorted Lists
 You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 Merge all the linked-lists into one sorted linked-list and return it.
 
@@ -32,3 +31,38 @@ k == lists.length
 lists[i] is sorted in ascending order.
 The sum of lists[i].length will not exceed 104.
 */
+
+let lists1 = [[1,4,5],[1,3,4],[2,6]];
+
+let lists2 = [];
+
+let lists3 = [[]];
+
+function listSorter(lists){
+    console.log("Problem array: ");
+    console.log(lists);
+    let newlist = [];
+
+    for(let i=0; i<lists.length; i++){
+        newlist = newlist.concat(lists[i])
+    }
+
+    newlist = newlist.sort(function(a, b){return a-b})
+    console.log("Merged Sorted Array: ");
+    console.log(newlist);
+    
+
+    //return newlist;
+}
+
+//Problem array: [ [ 1, 4, 5 ], [ 1, 3, 4 ], [ 2, 6 ] ]
+//Merged Sorted Array:[1, 1, 2, 3, 4, 4, 5, 6]
+listSorter(lists1) 
+
+//Problem array: []
+//Merged Sorted Array: []
+listSorter(lists2)
+
+//Problem array: [[]]
+//Merged Sorted Array: []
+listSorter(lists3)
